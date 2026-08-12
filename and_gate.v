748444f57115dc837ffd9ef7663fc76224c1675b
S P SAI SAHARSH <saisaharsh512@gmail.com>
2026-08-12 17:04:44 +0530

    Add files via upload

--- and_gate.v
+++ and_gate.v
@@ -1,7 +1,21 @@
-module and_gate(
-input a,
-input b,
-output y
-);
+module and_gate(a, b, y, o,n,r,t,xo,xn);
+
+input a;
+input b;
+output y;
+output o;
+output n;
+output r;
+output t;
+output xo;
+output xn;
+
 assign y = a & b;
+assign o=a|b;
+assign n=~(a&b);
+assign r=~(a|b);
+assign t=~a;
+assign xo=a^b;
+assign xn=~(a^b);
+
 endmodule
